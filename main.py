4272f4ee3e89693377ca4a4be0c44e589b052dbb
mosTafa <mostafa1382ma@gmail.com>
2020-08-16 05:45:54 +0430

    Test

---
+++ main.py
@@ -0,0 +1,9 @@
+from pyrogram import Client,Filters
+import settings
+import os
+print(os.getenv("NAME"))
+
+
+app = Client('BAAr_YwxVyqZBDlMlalJP8OHZb66X74WH25PlxedNgTpjru4474dYQ27YSXQ4t1ZlPU01FUg9uCLfL_fdD-mDWfR-bcTzpTz-71Hg-SXZnMhUKtWMZBAzy2STbEMQfcLj0Qwg0vAqQfVdBrDsRNSIUKmJRXiGEYrToQu5tu1Ayw_Nxh0FDwAt7xnLeDT1RyfIJdqToWkeHoaf6dXLYNqrfSgnb75ANMj51O1tr6Pd-OtENRBCPX4MVXMP6aj2C4Y9jpMkqSyzGM5GGCK3U7iUi_cKDkqNZRXDBHIRFpgeiVq9j3g6Alsc22Qrf9maiLhSnHPdk-HqLLISsCF_Zwud0qtN5VCwwA',app_version="1.0.0",device_model="@beQrity Self Bot",system_version="v1.0")
+
+app.run()
