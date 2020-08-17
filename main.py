f6384ebf3473fb5bb56034958cfecb1d81b07c30
mosTafa <mostafa1382ma@gmail.com>
2020-08-17 17:09:51 +0430

    edit session

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import logging
 logging.basicConfig(
     format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
 
-app = Client('rain',app_version="1.0.0",device_model="@beQrity Self Bot",system_version="v1.0",)
+app = Client('rush',app_version="1.0.0",device_model="@beQrity Self Bot",system_version="v1.0",)
 
 app.run()
